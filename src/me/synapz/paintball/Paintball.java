8f8bf98b9f2f9f52d16d521a87ce7a3ee4e866e3
Dart2112 <DNAblue2112@gmail.com>
2015-12-13 16:16:51 +1030

    Moved everything into PaintballFile
    We now call add and remove on load and save of file.
    Added usage of the offline backup.
    Added messages for if loading fails.

--- src/me/synapz/paintball/Paintball.java
+++ src/me/synapz/paintball/Paintball.java
@@ -3,7 +3,6 @@ package me.synapz.paintball;
 import me.synapz.paintball.commands.CommandManager;
 import me.synapz.paintball.events.*;
 import me.synapz.paintball.storage.PlayerData;
-import me.synapz.paintball.storage.SQLStatisticsStorage;
 import me.synapz.paintball.storage.Settings;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,16 +10,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Paintball extends JavaPlugin {
 
     PlayerData data;
-    SQLStatisticsStorage sql;
 
     @Override
     public void onEnable() {
         Team.loadTeamColors();
 
-        data = new PlayerData(this);
+        this.data = new PlayerData(this);
         Settings.getSettings();
 
-        loadSQL();
+        this.loadSQL();
 
         ArenaManager.getArenaManager().setup();
         CommandManager commandManager = new CommandManager();
@@ -32,18 +30,17 @@ public class Paintball extends JavaPlugin {
         Bukkit.getServer().getPluginManager().registerEvents(new ChatSystem(), this);
         Bukkit.getServer().getPluginManager().registerEvents(new LeaderboardSigns(), this);
 
-        getCommand("paintball").setExecutor(commandManager);
+        this.getCommand("paintball").setExecutor(commandManager);
     }
 
     public void loadSQL() {
-        if (getConfig().getBoolean("SQL")) {
-            sql = new SQLStatisticsStorage(this);
-            String host = getConfig().getString("SQL-Settings.host");
-            Integer port = getConfig().getInt("SQL-Settings.port");
-            String user = getConfig().getString("SQL-Settings.user");
-            String pass = getConfig().getString("SQL-Settings.pass");
-            String database = getConfig().getString("SQL-Settings.database");
-            sql.setupSQL(host, port, user, pass, database);
+        if (this.getConfig().getBoolean("SQL")) {
+            String host = this.getConfig().getString("SQL-Settings.host");
+            Integer port = this.getConfig().getInt("SQL-Settings.port");
+            String user = this.getConfig().getString("SQL-Settings.user");
+            String pass = this.getConfig().getString("SQL-Settings.pass");
+            String database = this.getConfig().getString("SQL-Settings.database");
+            this.data.setupSQL(host, port, user, pass, database);
         }
     }
 
@@ -54,6 +51,6 @@ public class Paintball extends JavaPlugin {
 
     // TODO: bad way of getting player data, find better way
     public PlayerData getPlayerData() {
-        return data;
+        return this.data;
     }
 }
--- src/me/synapz/paintball/storage/PaintballFile.java
+++ src/me/synapz/paintball/storage/PaintballFile.java
@@ -4,50 +4,147 @@ package me.synapz.paintball.storage;
 import me.synapz.paintball.Message;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Base64;
+import java.util.Set;
 
 public abstract class PaintballFile {
 
-    private FileConfiguration fileConfig;
-    private File file;
-    private String name;
+    private final FileConfiguration fileConfig;
+    private final File file;
+    private final String name;
+    /**
+     * SQL Stuffs
+     **/
 
+    Boolean sql = false;
+    String host;
+    Integer port;
+    String username;
+    String password;
+    String database;
     public PaintballFile(Plugin pb, String name) {
         this.name = name;
 
-        file = new File(pb.getDataFolder(), name);
+        this.file = new File(pb.getDataFolder(), name);
 
-        if (!file.exists()) {
+        if (!this.file.exists()) {
             try {
-                file.createNewFile();
+                this.file.createNewFile();
             } catch (IOException e) {
                 Message.getMessenger().msg(Bukkit.getConsoleSender(), false, ChatColor.RED, "", "Could not create " + name + ". Stack trace: ");
                 e.printStackTrace();
             }
         }
-        fileConfig = YamlConfiguration.loadConfiguration(file);
-        saveFile();
+        this.fileConfig = YamlConfiguration.loadConfiguration(this.file);
+        this.saveFile();
     }
 
     public void saveFile() {
         try {
-            fileConfig.save(file);
+            if (this.sql && this.file.getName().contains("playerdata")) {
+                this.removeStats(this.fileConfig).save(this.file);
+            }
+            this.fileConfig.save(this.file);
         } catch (Exception e) {
-            Message.getMessenger().msg(Bukkit.getConsoleSender(), false, ChatColor.RED, "Could not save " + name + ".", "", "Stack trace");
+            Message.getMessenger().msg(Bukkit.getConsoleSender(), false, ChatColor.RED, "Could not save " + this.name + ".", "", "Stack trace");
             e.printStackTrace();
         }
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public FileConfiguration getFileConfig() {
-        return fileConfig;
+        if (this.sql && this.file.getName().contains("playerdata")) {
+            return this.addStats(this.fileConfig);
+        }
+        return this.fileConfig;
+    }
+
+    public void setupSQL(String host, Integer port, String username, String password, String database) {
+        this.sql = true;
+        this.host = host;
+        this.port = port;
+        this.username = username;
+        this.password = password;
+        this.database = database;
+        executeQuery("CREATE DATABASE IF NOT EXISTS " + database);
+        executeQuery("CREATE TABLE IF NOT EXISTS Paintball_Stats (id INTEGER not null,stats STRING,PRIMARY KEY (id))");
+    }
+
+    public FileConfiguration removeStats(FileConfiguration yaml) {
+        Set<String> keys = yaml.getConfigurationSection("Player-Data").getKeys(false);
+        YamlConfiguration statsYaml = new YamlConfiguration();
+        for (String key : keys) {
+            ConfigurationSection stats = yaml.getConfigurationSection(key + ".Stats");
+            String path = stats.getCurrentPath();
+            statsYaml.set(path, stats);
+            yaml.set(path, null);
+        }
+        byte[] byteArray = statsYaml.saveToString().getBytes();
+        String encoded = Base64.getEncoder().encode(byteArray).toString();
+        yaml.set("Stats", encoded);
+        executeQuery("INSERT INTO Paintball_Stats (id,stats) VALUES (1," + encoded + ")");
+        return yaml;
+    }
+
+    public FileConfiguration addStats(FileConfiguration yaml) {
+        YamlConfiguration statsYaml = new YamlConfiguration();
+        try {
+            Connection conn;
+            conn = DriverManager.getConnection(host, username, password);
+            PreparedStatement sql = conn.prepareStatement("SELECT statsFROM `Paintball_Stats` WHERE id = '1';");
+            ResultSet result = sql.executeQuery();
+            result.next();
+            String base64Stats = result.getString("stats");
+            String yamlString = Base64.getDecoder().decode(base64Stats.getBytes()).toString();
+            statsYaml.loadFromString(yamlString);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Bukkit.getLogger().info("SQL connection failed! Using offline backup until we can connect again");
+            if (yaml.contains("Stats")) {
+                String base64Stats = yaml.getString("Stats");
+                String yamlString = Base64.getDecoder().decode(base64Stats.getBytes()).toString();
+                try {
+                    statsYaml.loadFromString(yamlString);
+                } catch (InvalidConfigurationException e1) {
+                    e1.printStackTrace();
+                    Bukkit.getLogger().severe("Failed to load offline config! Please check SQL connection and playerdata file!");
+                }
+            } else {
+                Bukkit.getLogger().severe("Statistics Down!! We have no SQL connection and don't have a backup of stats!");
+            }
+        }
+        Set<String> keys = statsYaml.getConfigurationSection("Player-Data").getKeys(false);
+        for (String key : keys) {
+            ConfigurationSection stats = statsYaml.getConfigurationSection(key + ".Stats");
+            String path = stats.getCurrentPath();
+            yaml.set(path, stats);
+        }
+        return yaml;
+    }
+
+    public void executeQuery(String query) {
+        Connection conn;
+        try {
+            conn = DriverManager.getConnection(host, username, password);
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.executeQuery();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
