4bcc407a03c2515387ecb25b2f4a2afecff2c1b6
Synapz1 <soldierboy64655@gmail.com>
2016-06-22 10:24:47 -0400

    Fixed build errors

--- src/me/synapz/paintball/events/ArenaBuyItemEvent.java
+++ src/me/synapz/paintball/events/ArenaBuyItemEvent.java
@@ -9,13 +9,19 @@ import org.bukkit.event.HandlerList;
 public class ArenaBuyItemEvent extends ArenaEvent {
 
     private CoinItem coinItem;
+    private ArenaPlayer arenaPlayer;
 
     public ArenaBuyItemEvent(ArenaPlayer arenaPlayer, CoinItem coinItem) {
         super(arenaPlayer, arenaPlayer.getArena());
         this.coinItem = coinItem;
+        this.arenaPlayer = arenaPlayer;
     }
 
     public CoinItem getCoinItem() {
         return this.coinItem;
+    }
+
+    public ArenaPlayer getArenaPlayer() {
+        return arenaPlayer;
     }
 }
--- src/me/synapz/paintball/events/ArenaClickItemEvent.java
+++ src/me/synapz/paintball/events/ArenaClickItemEvent.java
@@ -18,4 +18,5 @@ public class ArenaClickItemEvent extends ArenaBuyItemEvent {
         return action;
     }
 
+
 }
